f79a879c2390d6ed39bd0924b8823a3890c5f7d2
mdrizwanuzzmann <mdrizwanuzzaman46@gmail.com>
2026-03-08 00:58:20 +0530

    ieie

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <footer>Designed and Developed by Rizwan</footer>;
+};
+
+export default Footer;
---
+++ frontend/src/components/WorkoutSessions.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const WorkoutSessions = () => {
+  return (
+    <section className="workout_session">
+      <div className="wrapper">
+        <h1>TOP WORKOUT SESSION</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          aperiam tenetur eaque obcaecati necessitatibus voluptatem sit culpa
+          consequatur!x
+        </p>
+        <img src="/img5.jpg" alt="workout" />
+      </div>
+
+      <div className="wrapper">
+        <h1>FEATURED BOOTCAMPS</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+          accusantium fugit incidunt odit, cum provident cupiditate? Rem,
+          tempore?
+        </p>
+        <div className="bootcamps">
+          <div>
+            <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex,
+              perspiciatis velit rerum quo est autem!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex,
+              perspiciatis velit rerum quo est autem!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex,
+              perspiciatis velit rerum quo est autem!
+            </p>
+          </div>
+          <div>
+            <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex,
+              perspiciatis velit rerum quo est autem!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WorkoutSessions;

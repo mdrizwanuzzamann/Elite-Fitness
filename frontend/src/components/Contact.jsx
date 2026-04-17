25d02252cbcd1889802fcaf18b6801249b51d8bb
mdrizwanuzzamann <mdrizwanuzzaman46@gmail.com>
2026-04-17 20:35:42 +0530

    iei

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        `http://localhost:4000/send/mail`,
+        `https://elite-fitness-frontend.onrender.com/send/mail`,
         {
           name,
           email,

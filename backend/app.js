e9c406377ebaf8f0b23da3d92caf9bb8b72bb380
mdrizwanuzzamann <mdrizwanuzzaman46@gmail.com>
2026-05-13 21:26:34 +0530

    er

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 app.use(
   cors({
-    origin: "https://elite-fitness-frontend.onrender.com",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
